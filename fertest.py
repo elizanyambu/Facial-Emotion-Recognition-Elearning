0054b1bd0c41ca70e5f963fa4fed1049886dfe63
elizanyambu <55143867+elizanyambu@users.noreply.github.com>
2021-01-27 21:42:18 +0100

    Add files via upload

---
+++ fertest.py
@@ -0,0 +1,38 @@
+# load json and create model
+from __future__ import division
+from keras.models import model_from_json
+import numpy as np
+
+json_file = open('fer.json', 'r')
+loaded_model_json = json_file.read()
+json_file.close()
+loaded_model = model_from_json(loaded_model_json)
+# load weights into new model
+loaded_model.load_weights("fer.h5")
+print("Loaded model from disk")
+
+truey=[]
+predy=[]
+x = np.load('./modXtest.npy')
+y = np.load('./modytest.npy')
+
+yhat= loaded_model.predict(x)
+yh = yhat.tolist()
+yt = y.tolist()
+count = 0
+
+for i in range(len(y)):
+    yy = max(yh[i])
+    yyt = max(yt[i])
+    predy.append(yh[i].index(yy))
+    truey.append(yt[i].index(yyt))
+    if(yh[i].index(yy)== yt[i].index(yyt)):
+        count+=1
+
+acc = (count/len(y))*100
+
+#saving values for confusion matrix and analysis
+np.save('truey', truey)
+np.save('predy', predy)
+print("Predicted and true label values saved")
+print("Accuracy on test set :"+str(acc)+"%")
